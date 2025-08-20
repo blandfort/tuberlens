d5808befb987bf18f7683b3515f39ded39a9f65c
Phil Blandfort <consulting@pblandfort.com>
2025-08-20 20:39:48 +0200

    showing progress for computing activations

--- src/potato/training.py
+++ src/potato/training.py
@@ -21,11 +21,34 @@ def train_probe(
     probe_description: str | None = None,
     use_store: bool = False,
 ):
+    """
+    Train a probe on a dataset.
+
+    NOTE: Some arguments currently only work for PyTorch probes.
+
+    Args:
+        train_dataset: The dataset to train the probe on.
+        validation_dataset: The dataset to validate the probe on.
+        model_name: The name of the model to use.
+        layer: The layer to use for the probe.
+        probe_spec: The specification of the probe to use.
+        verbose: Whether to print verbose output.
+        ending_tokens_to_ignore: The number of tokens to ignore at the end of the input.
+        start_turn_index: The index of the turn to start the probe at.
+        end_turn_index: The index of the turn to end the probe at.
+        apply_transformations_to_validation_dataset: Whether to apply ending_tokens_to_ignore and turn indices to the validation dataset.
+        pos_class_label: The label of the positive class.
+        neg_class_label: The label of the negative class.
+        probe_description: The description of the probe.
+        use_store: Whether to use the store trained probes using the probe store.
+    """
     model = LLMModel.load(model_name)
+    print("Computing activations for training dataset...")
     activations = model.get_activations(
         train_dataset.inputs,
         layer=layer,
         ending_tokens_to_ignore=ending_tokens_to_ignore,
+        show_progress=True,
     )
     if start_turn_index is not None or end_turn_index is not None:
         activations = filter_activations_by_turns(
@@ -44,12 +67,14 @@ def train_probe(
     print(activations.activations.shape)
 
     if validation_dataset is not None:
+        print("Computing activations for validation dataset...")
         activations = model.get_activations(
             validation_dataset.inputs,
             layer=layer,
             ending_tokens_to_ignore=ending_tokens_to_ignore
             if apply_transformations_to_validation_dataset
             else 0,
+            show_progress=True,
         )
         if apply_transformations_to_validation_dataset and (
             start_turn_index is not None or end_turn_index is not None
